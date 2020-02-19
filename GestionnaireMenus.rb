c642fb4a6936e0227868157cac420dc8daf6a989
Tan Eric <s173965@ic2s124-08.univ-lemans.fr>
2020-02-19 15:11:32 +0100

    hey hey hey

---
+++ GestionnaireMenus.rb
@@ -0,0 +1,42 @@
+load "Menu.rb"
+load "SousMenu.rb"
+
+require "gtk3"
+
+class GestionnaireMenus
+
+    attr :box, true
+
+    def GestionnaireMenus.creer(window)
+        new(window)
+      end
+ 
+    def initialize(window)
+        @window = window
+
+        @box =  Gtk::Box.new(:horizontal)
+        @window.add(@box)
+
+        @menu = Menu.creer(self)
+        @sousMenu = SousMenu.creer(self, @menu)
+        @menu.sousMenu = @sousMenu
+        @menu.afficheToi()
+    end
+
+    def changerMenu(menuAff, menuEnl)
+        menuEnl.enleveToi()
+        menuAff.afficheToi()
+        @window.show_all
+    end
+
+end
+
+window = Gtk::Window.new("First Menu")
+window.set_size_request(400, 400)
+window.set_border_width(10)
+gMenu = GestionnaireMenus.new(window)
+
+window.signal_connect("delete-event") { |_widget| Gtk.main_quit }
+window.show_all
+
+Gtk.main
---
+++ Menu.rb
@@ -0,0 +1,61 @@
+load "SousMenu.rb"
+
+require "gtk3"
+
+class Menu
+
+    attr :sousMenu, true
+    private_class_method :new
+
+    def Menu.creer(gMenu)
+        new(gMenu)
+    end
+
+    def initialize(gMenu)
+        @gMenu = gMenu
+        @hBox = gMenu.box
+        @vBox1 = Gtk::Box.new(:vertical)
+        @vBox2 = Gtk::Box.new(:vertical)
+        @button1 = Gtk::Button.new(:label => 'Tutoriel')
+        @button1.signal_connect('clicked') {
+            print "Bonjour tout le monde\n"
+        }
+        @button2 = Gtk::Button.new(:label => 'Mode de jeu')
+        @button2.signal_connect('clicked') {
+            print "Bonjour tout le monde\n"
+        }
+        @button3 = Gtk::Button.new(:label => 'Boutique')
+        @button3.signal_connect('clicked') {
+            print "Bonjour tout le monde\n"
+        }
+        @button4 = Gtk::Button.new(:label => 'Paramètres')
+        @button4.signal_connect('clicked') {
+            print "Bonjour tout le monde\n"
+        }
+        @button5 = Gtk::Button.new(:label => 'Règles et Techniques')
+        @button5.signal_connect('clicked') {
+            print "Bonjour tout le monde\n"
+        }
+        @button6 = Gtk::Button.new(:label => 'Quitter')
+        @button6.signal_connect('clicked') {
+            Gtk.main_quit
+        }
+
+    end
+
+    def afficheToi()
+        @vBox1.add(@button1)
+        @vBox1.add(@button2)
+        @vBox1.add(@button3)
+        @vBox2.add(@button4)
+        @vBox2.add(@button5)
+        @vBox2.add(@button6)
+        @hBox.add(@vBox1)
+        @hBox.add(@vBox2)
+    end
+
+    def enleveToi()
+      @hBox.remove(@vBox1)
+      @hBox.remove(@vBox2)
+    end
+end
